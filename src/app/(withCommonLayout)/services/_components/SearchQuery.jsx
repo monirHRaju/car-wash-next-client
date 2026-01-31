f3fdb80b3bb1c06f35965963bf2ee7056131942b
Monir Hossain Raju <monirhraju@gmail.com>
2026-01-31 10:26:04 +0600

    service search with text implemented-- done

--- src/app/(withCommonLayout)/services/_components/SearchQuery.jsx
+++ src/app/(withCommonLayout)/services/_components/SearchQuery.jsx
@@ -1,28 +1,38 @@
+"use client"
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
+
+
+
 const SearchQuery = () => {
+  const router = useRouter()
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  console.log({router, searchParams, pathname})
+
+  const handleSearchQuery= (e) => {
+    e.preventDefault();
+    const searchTerm = e.target.search?.value;
+    const params = new URLSearchParams(searchParams);
+
+    if (searchTerm) {
+      params.set("searchTerm", searchTerm);
+    } else {
+      params.delete("searchTerm");
+    }
+
+    router.push(`${pathname}?${params.toString()}`);
+    console.log({'searchTerm': searchTerm, 'params': params})
+
+  }
   return (
-    <form>
+    <form onSubmit={handleSearchQuery}>
       <div className="join">
         <div>
           <label className="input validator join-item">
-            <svg
-              className="h-[1em] opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-              </g>
-            </svg>
-            <input type="input" placeholder="search term" required />
+            <input type="input" placeholder="search" name="search" />
           </label>
           <div className="validator-hint hidden">Type something to search.</div>
         </div>
--- src/app/(withCommonLayout)/services/page.jsx
+++ src/app/(withCommonLayout)/services/page.jsx
@@ -3,35 +3,47 @@ import React from 'react';
 import ServiceCard from './_components/ServiceCard';
 import SearchQuery from './_components/SearchQuery';
 
-const getAllServices = async () => {
-        const res = await fetch('https://car-washing-system-cleanify-server.vercel.app/api/v1/services')
-        await new Promise((resolve) => 
-            setTimeout(() => {
-                resolve()
-            }), 3000
-        )
+const getAllServices = async (searchParams) => {
+        const getParams = new URLSearchParams(searchParams).toString();
+
+        console.log('getParams data', getParams)
+        const res = await fetch(`https://car-washing-system-cleanify-server.vercel.app/api/v1/services?${getParams}`, {
+            cache: 'no-store'
+        })
+        // await new Promise((resolve) => 
+        //     setTimeout(() => {
+        //         resolve()
+        //     }), 3000
+        // )
         const data = await res.json()
         return data
     }
 
-const Services = async () => {
+const Services = async ({searchParams}) => {
     
-    const services = await getAllServices()
-
-    // console.log(services?.data)
+    const sParams = await searchParams
+    // console.log('search term',searchTerm)
     
+    const servicesList = await getAllServices({...sParams})
+ 
     return (
         <div className='py-7'>
             <Container>
                 <h1 className='text-4xl font-bold mb-6'>Services</h1>
                 <SearchQuery/>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+
+                {servicesList?.meta?.total === 0 ? <div className='flex flex-col justify-center items-center h-96 mt-5'>
+                        <div className='text-4xl font-semibold'>No Data Found.</div>
+                    </div>
+
+               : <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-5'>
                     {
-                        services?.data?.map(service => {
-                            return <ServiceCard key={service._id} service={service} />
-                        })
-                    }
-                </div>
+                            servicesList?.data?.map(service => {
+                                return <ServiceCard key={service._id} service={service} />
+                            })
+                        }
+                    </div> 
+                }
             </Container>
         </div>
     );
